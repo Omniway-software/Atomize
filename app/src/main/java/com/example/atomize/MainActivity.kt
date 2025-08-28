6687f835da4880b51a43ba8f4cf2101eb8b91ac6
dusanrsc <dusanrosic25.06.1997@gmail.com>
2025-08-28 15:33:54 +0200

    Atomize 3 - Created project structure.

--- app/src/main/java/com/example/atomize/MainActivity.kt
+++ app/src/main/java/com/example/atomize/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.atomize.ui.theme.AtomizeTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.atomize.ui.theme.home.HomeFragment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +22,30 @@ class MainActivity : ComponentActivity() {
         setContent {
             AtomizeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AppNavigation(modifier = Modifier.padding(paddingValues = innerPadding))
                 }
             }
         }
     }
 }
 
+// Application Navigation.
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppNavigation(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AtomizeTheme {
-        Greeting("Android")
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "home",
+            modifier = Modifier.padding(paddingValues = innerPadding),
+        ) {
+            // Defining Routes Section.
+            composable(route = "home") {
+                HomeFragment(navController)
+            }
+        }
     }
 }
