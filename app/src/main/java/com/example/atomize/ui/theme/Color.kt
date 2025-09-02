d1d93ee2df935e6c4011edd518a729f54d9c9729
Dušan Rosić (SRB) <149257819+dusanrsc@users.noreply.github.com>
2025-09-02 19:07:21 +0200

    Atomize 4 - Setup theme

--- app/src/main/java/com/example/atomize/ui/theme/Color.kt
+++ app/src/main/java/com/example/atomize/ui/theme/Color.kt
@@ -11,5 +11,30 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 // Custom Made Colors Section.
-val BeautifulGreen = Color(color = 0xFF40DE23)
-val DefaultAppBackgroundColor = BeautifulGreen
+// Primary Colors.
+val PrimaryGreen = Color(color = 0xFF4CAF50)
+val LightGreen = Color(color = 0xFF81C784)
+val VeryLightGreen  = Color(color = 0xFFE8F5E8)
+
+// Accent Colors.
+val Orange = Color(color = 0xFFFF7043)
+val Red = Color(color = 0xFFE53935)
+
+// Neutral Colors.
+val LightGray = Color(color = 0xFFF5F5F5)
+val MediumGray = Color(color = 0xFF9E9E9E)
+val DarkGray = Color(color = 0xFF424242)
+val White = Color(color = 0xFFFFFFFF)
+
+// Text Colors.
+val PrimaryTextColor = Color(color = 0xFF212121)
+val SecondaryTextColor = Color(color = 0xFF757575)
+val DisabledTextColor = Color(color = 0xFFBDBDBD)
+
+// Mapping Functionalities.
+val CalendarActiveDay = PrimaryGreen
+val CalendarInactiveDay = LightGreen
+val TaskCompleted = PrimaryGreen
+val TaskIncomplete = MediumGray
+val FlameIcon = Orange
+val FlameNumber = Red
--- app/src/main/java/com/example/atomize/ui/theme/Type.kt
+++ app/src/main/java/com/example/atomize/ui/theme/Type.kt
@@ -2,33 +2,125 @@ package com.example.atomize.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.atomize.R
 
-// Set of Material typography styles to start with
+val ManropeFontFamily = FontFamily(
+    Font(resId = R.font.manrope_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.manrope_bold, weight = FontWeight.Bold)
+)
+
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = ManropeFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+        letterSpacing = (-0.25).sp,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = ManropeFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = ManropeFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    headlineLarge = TextStyle(
+        fontFamily = ManropeFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = ManropeFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp,
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = ManropeFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp,
+    ),
+
+    titleLarge = TextStyle(
+        fontFamily = ManropeFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = ManropeFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = ManropeFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp,
+    ),
+
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = ManropeFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        letterSpacing = 0.5.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = ManropeFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = ManropeFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp,
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = ManropeFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = ManropeFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp,
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+        fontFamily = ManropeFontFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.5.sp,
     )
-    */
 )
